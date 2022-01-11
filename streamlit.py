72b0fc568e2be114bbb455e4f03b1e5bc439af0d
maravanboeckel <89785847+maravanboeckel@users.noreply.github.com>
2022-01-11 15:11:51 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -112,62 +112,62 @@ if rad == 'Kaart':
                                     """
    
 
-                                    css = """
+                            css = """
 
-                                    <style type='text/css'>
-                                    .maplegend {
-                                    z-index:9999;
-                                    float:right;
-                                    background-color: rgba(255, 255, 255, 1);
-                                    border-radius: 5px;
-                                    border: 2px solid #bbb;
-                                    padding: 10px;
-                                    font-size:12px;
-                                    positon: relative;
-                                    }
-                                    .maplegend .legend-title {
-                                    text-align: left;
-                                    margin-bottom: 5px;
-                                    font-weight: bold;
-                                    font-size: 90%;
-                                    }
-                                    .maplegend .legend-scale ul {
-                                    margin: 0;
-                                    margin-bottom: 5px;
-                                    padding: 0;
-                                    float: left;
-                                    list-style: none;
-                                    }
-                                    .maplegend .legend-scale ul li {
-                                    font-size: 80%;
-                                    list-style: none;
-                                    margin-left: 0;
-                                    line-height: 18px;
-                                    margin-bottom: 2px;
-                                    }
-                                    .maplegend ul.legend-labels li span {
-                                    display: block;
-                                    float: left;
-                                    height: 16px;
-                                    width: 30px;
-                                    margin-right: 5px;
-                                    margin-left: 0;
-                                    border: 0px solid #ccc;
-                                    }
-                                    .maplegend .legend-source {
-                                    font-size: 80%;
-                                    color: #777;
-                                    clear: both;
-                                    }
-                                    .maplegend a {
-                                    color: #777;
-                                    }
-                                    </style>
-                                    """
+                            <style type='text/css'>
+                             .maplegend {
+                             z-index:9999;
+                             float:right;
+                             background-color: rgba(255, 255, 255, 1);
+                             border-radius: 5px;
+                             border: 2px solid #bbb;
+                             padding: 10px;
+                             font-size:12px;
+                             positon: relative;
+                              }
+                             .maplegend .legend-title {
+                             text-align: left;
+                             margin-bottom: 5px;
+                             font-weight: bold;
+                             font-size: 90%;
+                              }
+                              .maplegend .legend-scale ul {
+                              margin: 0;
+                              margin-bottom: 5px;
+                              padding: 0;
+                              float: left;
+                              list-style: none;
+                              }
+                              .maplegend .legend-scale ul li {
+                              font-size: 80%;
+                              list-style: none;
+                              margin-left: 0;
+                              line-height: 18px;
+                              margin-bottom: 2px;
+                              }
+                              .maplegend ul.legend-labels li span {
+                              display: block;
+                              float: left;
+                              height: 16px;
+                              width: 30px;
+                              margin-right: 5px;
+                              margin-left: 0;
+                              border: 0px solid #ccc;
+                              }
+                              .maplegend .legend-source {
+                              font-size: 80%;
+                              color: #777;
+                              clear: both;
+                              }
+                              .maplegend a {
+                              color: #777;
+                              }
+                              </style>
+                              """
 
-                                    folium_map.get_root().header.add_child(folium.Element(script + css))
+                              folium_map.get_root().header.add_child(folium.Element(script + css))
 
-                                    return folium_map
+                              return folium_map
 
 
          map_houten= folium.Map(location=[52.015154,5.171879], zoom_start = 15)
