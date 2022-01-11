5d734837df071e93fcd981e23f884989f3d7d008
maravanboeckel <89785847+maravanboeckel@users.noreply.github.com>
2022-01-11 14:36:17 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,7 +20,7 @@ import plotly.express as px
 st.title('Kaart met scheefstand lantaarnpalen in Houten')
 
 vergelijk1=pd.read_csv('Data_kaart.csv')
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 #rad = st.sidebar.radio(options=('Grafiek','Kaart'),label='Selecteer')
 st.sidebar.subheader('Gemaakt door:')
 st.sidebar.write('Mara van Boeckel')
