7561478de5898ee136a4386bd8c2d87b8df0ee43
maravanboeckel <89785847+maravanboeckel@users.noreply.github.com>
2022-01-11 15:02:13 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -32,7 +32,7 @@ st.sidebar.markdown('#')
 
 # In[ ]:
 if rad == 'Grafiek':
-         st.title('Grafiek')
+         st.title('Grafiek van de scheefstande (waterpas en het algoritme)')
          fig = px.line(vergelijk1, x="lantaarnpaal_nummer", y=["scheefstand","scheefstand_tov_kader"],labels={
                   "value": "Scheefstand (graden)", 'variable':''},
                        title='Scheefstand waterpas en algoritme')
