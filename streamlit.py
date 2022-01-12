773fc2d8786acdde0bdb503adcfcd0e6fecede1c
rhoderebel <91129594+rhoderebel@users.noreply.github.com>
2022-01-12 11:30:11 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -200,7 +200,7 @@ if rad == 'Kaart':
                   marker.add_to(effecten[1])
                   effecten[1].add_to(map_houten)
     
-         map_houten = add_categorical_legend(map_houten, 'Scheefstand',
+         map_legend = add_categorical_legend(map_houten, 'Scheefstand',
                                              colors=['darkred','red', 'orange', 'green'],
                                              labels=['Meer dan 6°', 'Tussen 3° en 6°', 'Tussen 1° en 3°', 'Minder dan 1°'])   
     
